af645750d64d48c41b417d68dbd862df41fa506f
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2024-10-04 11:06:14 +0000

    End of lesson 13

--- script/contacts.js
+++ script/contacts.js
@@ -1,3 +1,38 @@
+
 let validate = () => {
-	// Fill in the necessary validation here
+let inputfirstName = document.getElementById("firstname");
+console.log("I got this " + inputfirstName.value);
+
+if (inputfirstName.value.length > 0) {
+	inputfirstName.classList = "form-control is-valid";
+	document.getElementById("firstnameError").classList = "text-danger d-none";
+} else {
+	inputfirstName.classList = "form-control is-invalid";
+	document.getElementById("firstnameError").classList = "text-danger";
 }
+
+let inputemail = document.getElementById("email");
+console.log("I got this " + inputemail.value);
+
+if (inputemail.value.length > 0) {
+	inputemail.classList = "form-control is-valid";
+	document.getElementById("emailError").classList = "text-danger d-none";
+} else {
+	inputemail.classList = "form-control is-invalid";
+	document.getElementById("emailError").classList = "text-danger";
+}
+
+
+
+
+if (document.getElementsByClassName("is-invalid").length > 0) {
+	console.log("Please check for any errors. This form will be deleted by 30 minutes if the error is not cleared")
+	} else {
+	console.log("Form is submitted! Please hold on for a reply from a online and vacant helper")
+	}
+}	
+				
+				
+			
+			  
+			 
