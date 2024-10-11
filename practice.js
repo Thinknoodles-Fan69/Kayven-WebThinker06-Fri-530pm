a5ac1d67d75e812188448149cfea86453c62626f
Thinknoodles-Fan69 <Kayvengst@gmail.com>
2024-10-11 10:58:07 +0000

    End of Lesson 14

---
+++ practice.js
@@ -0,0 +1,21 @@
+let my_home_address = "Ohio St 50 Blk 48 #58-567 470045"
+const My_NRIC = "69696969";
+
+console.log(my_home_address);
+console.log(My_NRIC)
+
+
+//old way
+function calculateMyIncome() {
+    console.log("$9,453,547,000");
+}
+
+
+//new way
+let calculateMyTax = () => {
+    console.log("$9354");
+}
+
+//call the function
+calculateMyIncome();
+calculateMyTax();
